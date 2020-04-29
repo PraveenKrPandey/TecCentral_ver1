9e9095c847eb233d462dbd49dddf364e40fa0067
PraveenKrPandey <prkumar24@yahoo.com>
2020-04-29 22:28:34 +0530

    commit1

---
+++ contactUs.php
@@ -0,0 +1,47 @@
+<!-- start contact us section -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <h2 class="text-center mb-4">Contact Us</h2>
+            <!--Start 1st column-->
+            <form action="" method="post">
+                <input type="text" class="form-control" name="name" placeholder="Name"><br>
+                <input type="text" class="form-control" name="Subject" placeholder="Subject"><br>
+                <input type="email" class="form-control" name="Email" placeholder="Email"><br>
+                <textarea name="message" class="form-control" placeholder="how can we help you?" style="height:150px;"></textarea>
+                <input type="submit" class="btn btn-warning" value="Send" name="Submit"><br><br>
+            </form>
+        </div>
+        <!--End of 1st column-->
+
+        <!--start of 2nd column-->
+        <div class="col-md-4 text-center">
+            <div class="ContacAddress"><strong>Headquarter:</strong><br>
+                TecCentral pvt Ltd, <br>
+                Hari Nagar, New Delhi<br>
+                Delhi-110064 <br>
+                phone: 882467286<br><br>
+                <!-- <a href="#" target="_blank">www.TecCentral.com</a><br> -->
+            </div>
+            <br>
+            <div class="ContacAddress">
+                <strong>Branch:</strong><br>
+                TecCentral pvt Ltd, <br>
+                shiv nagar, Pune <br>
+                Maharastra - 546465 <br>
+                phone: 882467286 <br><br>
+                <!-- <a href="#" target="_blank">www.TecCentral.com</a> -->
+            </div>
+            <br>
+            <div class="ContacAddress">
+                <strong>Branch:</strong><br>
+                TecCentral pvt Ltd, <br>
+                Indiranagar, Bangalore <br>
+                karnataka - 885888 <br>
+                phone: 882467286 <br>
+                <br>
+            </div>
+        </div>
+        <!--end of 2nd column-->
+    </div>
+</div> <!-- end contact us section-->
---
+++ userRegistration.php
@@ -0,0 +1,59 @@
+<?php //include('dbConnetion.php');
+
+if (isset($_REQUEST['rSignup'])) {
+    if (($_REQUEST['rName'] == "") || ($_REQUEST['rEmail'] == "") || ($_REQUEST['rPassword'] == "")) {
+        $regmsg = '<div class="alert alert-warning mt-2" role="alert">All Fields are Required</div>';
+    } else {
+        // $rName = $_REQUEST['rName'];
+        // $rEmail = $_REQUEST['rEmail'];
+        // $rPassword = $_REQUEST['rPassword'];
+        // $sql = "insert into requesterlogin(r_name, r_email, r_password) VALUES ('$rName', '$rEmail','$rPassword')";
+
+        // try {
+        //     if ($conn->query($sql) == true) {
+        $regmsg = '<div class="alert alert-success mt-2" role="alert">Account  Succefully Created</div>';
+        // } else {
+
+        // $regmsg = '<div class="alert alert-danger mt-2" role="alert">Unknown Error</div>';
+        // }
+
+        // } catch (Exception $e) {
+        // echo "i dont know";
+        // }
+
+    }
+}
+
+
+
+// echo "$rName<br>";
+// echo "$rEmail<br>";
+// echo "$rPassword<br>";
+?>
+
+<!-- start registration form -->
+<div class="container pt-5 id=" registration">
+    <h2 class="text-center">Create an Account</h2>
+    <div class="row mt-4 mb-4">
+        <div class="col-md-6 offset-md-3">
+            <form action="" class="shadow-lg p-4" method="post">
+                <div class="form-group">
+                    <i class="fas fa-user"></i><label for="name" class="font-weight-bold pl-2">Name</label><input type="text" class="form-control" placeholder="Name" name="rName">
+                </div>
+                <div class="form-group">
+                    <i class="fas fa-user"></i><label for="email" class="font-weight-bold pl-2">Email</label><input type="Email" class="form-control" placeholder="Email" name="rEmail">
+                    <small class="form-text">We'll never share your email with anyone</small>
+                </div>
+                <div class="form-group">
+                    <i class="fas fa-key"></i><label for="pass" class="font-weight-bold pl-2">Password</label><input type="Password" class="form-control" placeholder="Password" name="rPassword">
+                </div>
+                <button type="submit" class="btn btn-primary mt-5 btn-block shadow-sm font-weight-bold" name="rSignup">sign Up</button>
+                <em style="font-size:10px;">Note-By clicking sign Up, you agree to our terms, Data policy and cookie policy</em>
+                <?php if (isset($regmsg)) {
+                    echo $regmsg;
+                } ?>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- end registration form container-->
