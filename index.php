e0fc6bc7d9eef5ffe7df0898a43c5da5490712fe
PraveenKrPandey <prkumar24@yahoo.com>
2020-04-30 21:18:13 +0530

    comit4

--- index.php
+++ index.php
@@ -209,11 +209,11 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 
-
+<!-- 
         <script src="js/jquery.min.js"></script>
         <script src="js/popper.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
-        <script src="js/all.min.js"></script>
+        <script src="js/all.min.js"></script> -->
     </body>
 
     </html>
